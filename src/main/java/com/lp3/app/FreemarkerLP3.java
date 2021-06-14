3686829ac066cac2cbb5be93f55dfb4c93a9265d
helder90 <48700290+helder90@users.noreply.github.com>
2021-06-14 12:40:54 -0300

    tudo ok

---
+++ src/main/java/com/lp3/app/FreemarkerLP3.java
@@ -0,0 +1,13 @@
+package com.lp3.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemarkerLP3 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreemarkerLP3.class, args);
+	}
+
+}
